e8422cda8351b65640b0a31dc5549f1c404ed89c
prince david <princiedavid@gmail.com>
2022-11-13 14:14:32 +0200

    .

--- app.mjs
+++ app.mjs
@@ -26,36 +26,21 @@ app.get("/download", async (req, res) => {
 
     if (!file) return res.status(404).json({ msg: "not found" });
 
-    const range = req.headers.range;
-    if (!range) return res.status(400).send("error");
-
     const title = file.title
       .replace(/[-&\/\\#, +()$~%.'":*?<>{}]/g, " ")
       .trim();
-    const contentType = file.file_type === "video" ? "video/mp4" : "audio/mp3";
 
-    const fileSize = file.file_size;
-    const chunkSize = 10 ** 6;
-    const start = Number(range.replace(/\D/g, ""));
-    const end = Math.min(start + chunkSize, fileSize - 1);
-    const contentLength = end - start + 1;
-
-    const headers = {
-      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
-      "Accept-Ranges": "bytes",
-      "Content-Length": contentLength,
-      "Content-Disposition": `attachment; filename=ytlink.tk - ${title}.${file.ext}`,
-      "Content-Type": contentType,
-    };
-
-    res.set(headers);
+    res.set({
+      "Content-Length": file.file_size,
+      "Content-Disposition": `attachment; filename=ydloder.tk - ${title}.${file.ext}`,
+    });
 
     const resp = await drive.files.get(
       {
         fileId: file.file_id,
         alt: "media",
       },
-      { responseType: "stream", headers: { range: { start, end } } }
+      { responseType: "stream" }
     );
 
     resp.data.pipe(res);
