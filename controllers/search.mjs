4d8a16f28589bc48eef924f9f417ee7721d55c9c
prince david <princiedavid@gmail.com>
2022-11-30 15:40:29 +0200

    .

--- controllers/search.mjs
+++ controllers/search.mjs
@@ -1,4 +1,7 @@
+import axios from "axios";
 import yts from "yt-search";
+
+const backend_2 = "https://pytvapp.herokuapp.com";
 
 export const searchVideo = async (req, res) => {
   try {
@@ -20,10 +23,21 @@ export const searchPlaylist = async (req, res) => {
     if (!term)
       return res.status(400).json({ details: "search term is needed" });
 
-    const result = await yts(term);
+    const { data } = await axios.get(
+      `${backend_2}/search/playlists?term=${term}`
+    );
 
-    res.status(200).json(result.playlists);
+    const modifield_results = data.map((result) => ({
+      listId: result.id,
+      title: result.title,
+      videoCount: result.videoCount,
+      author: result.channel,
+      thumbnail: result.thumbnails.at(-1).url,
+    }));
+
+    res.status(200).json(modifield_results);
   } catch (error) {
+    console.log(error.message);
     res.status(500).json({ msg: "something went wrong" });
   }
 };
